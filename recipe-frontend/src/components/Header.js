e79da42cf2c2085a16658f888fde82d66f4cdb84
bako viktor <bako.viktor@promenade.hu>
2022-06-27 21:48:07 +0200

    conditional links && logout

--- recipe-frontend/src/components/Header.js
+++ recipe-frontend/src/components/Header.js
@@ -1,18 +1,21 @@
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
-import { logout } from "../features/auth/authSlice";
+import { NavLink, useNavigate } from "react-router-dom";
+import { logout, reset } from "../features/auth/authSlice";
 import "../style/nav.css";
 import { Spinner } from "./Spinner";
 
 const Header = () => {
   const dispatch = useDispatch();
   const { user, isLoading } = useSelector((state) => state.auth);
-
+  const navigate = useNavigate();
   const handleLogout = (e) => {
     e.preventDefault();
     dispatch(logout());
+    dispatch(reset());
+    if (!user) {
+      navigate("/login");
+    }
   };
-  if (isLoading) return <Spinner />;
 
   return (
     <header className="bg-indigo-700">
@@ -20,28 +23,29 @@ const Header = () => {
         <div className=" flex items-center pl-5">
           <NavLink
             to="/"
-            className="hover:bg-indigo-900 p-2 flex items-center justify-center hover:rounded-md"
+            className=" hover:bg-indigo-900 p-2 flex items-center justify-center hover:rounded-md"
           >
             Home
           </NavLink>
         </div>
         <div className="flex gap-10 pr-6">
-          {!user ? (
+          {!user && (
             <>
               <NavLink
                 to="/login"
-                className="hover:rounded-md p-2 hover:bg-indigo-900 "
+                className=" hover:rounded-md p-2 hover:bg-indigo-900 "
               >
                 Login
               </NavLink>
               <NavLink
                 to="/register"
-                className="hover:rounded-md p-2 hover:bg-indigo-900"
+                className=" hover:rounded-md p-2 hover:bg-indigo-900"
               >
                 Register
               </NavLink>
             </>
-          ) : (
+          )}
+          {user && (
             <button
               onClick={(e) => handleLogout(e)}
               className="hover:rounded-md p-2 hover:bg-indigo-900"
