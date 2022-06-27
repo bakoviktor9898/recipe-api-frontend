533140cb7f99b54fd884f19e8b12f9a1a4e731f0
bako viktor <bako.viktor@promenade.hu>
2022-06-27 21:49:56 +0200

    added auth redirect

--- recipe-frontend/src/pages/Home.jsx
+++ recipe-frontend/src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 
 const Home = () => {
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user) navigate("/login");
+  }, [navigate, user]);
+
   return (
     <div>
       <h1>Home page</h1>
