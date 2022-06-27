9fd6ad82be8319c0d836548cfa294fe8db4c9c4c
bako viktor <bako.viktor@promenade.hu>
2022-06-27 21:48:34 +0200

    fixed size

--- recipe-frontend/src/components/Spinner.js
+++ recipe-frontend/src/components/Spinner.js
@@ -1,9 +1,9 @@
 export const Spinner = () => {
   return (
-    <div className="grid place-items-center">
+    <div className="grid place-items-center h-full">
       <svg
         role="status"
-        className="w-12 h-12 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className="w-24 h-24 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
