ff6c6c53a9caa075bd7f42d9d27ee8e0c46bccaa
bako viktor <bako.viktor@promenade.hu>
2022-06-27 21:52:22 +0200

    thunk middleware && persist store

--- recipe-frontend/src/app/store.js
+++ recipe-frontend/src/app/store.js
@@ -1,8 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
   },
+  middleware: [thunk],
 });
+
+export const persistor = persistStore(store);
+
+// store.dispatch(getUser());
