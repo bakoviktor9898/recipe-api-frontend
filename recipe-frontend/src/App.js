010fa0ede135327e8e4d686b7776452d7d7e238a
bako viktor <bako.viktor@promenade.hu>
2022-06-27 21:54:05 +0200

    fullscreen for the app

--- recipe-frontend/src/App.js
+++ recipe-frontend/src/App.js
@@ -4,23 +4,17 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <div className="App h-full">
+    <div className="App h-screen w-full">
       <BrowserRouter>
         <Header />
         <Routes>
-          {!false && (
-            <>
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-            </>
-          )}
-
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
