4687ea4f9a2d412748415d18ed7beeab280df6ad
bako viktor <bako.viktor@promenade.hu>
2022-06-28 21:22:32 +0200

    dispatch all recipe

--- recipe-frontend/src/pages/Home.jsx
+++ recipe-frontend/src/pages/Home.jsx
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
+import { getAllRecipes } from "../features/recipe/recipeSlice";
 
 const Home = () => {
   const { user } = useSelector((state) => state.auth);
-
+  const { recipes } = useSelector((state) => state.recipe);
+  const [re, setRe] = useState([]);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   useEffect(() => {
     if (!user) navigate("/login");
-  }, [navigate, user]);
+    dispatch(getAllRecipes());
+  }, [navigate, user, dispatch]);
+
+  useEffect(() => {
+    setRe(recipes);
+  }, [recipes]);
 
   return (
     <div>
       <h1>Home page</h1>
+      <div></div>
     </div>
   );
 };
