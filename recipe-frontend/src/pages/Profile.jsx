69bc628b05479219c52b997610eb59e51c922c82
bako viktor <bako.viktor@promenade.hu>
2022-06-28 21:23:09 +0200

    check auth conditionally redirect

--- recipe-frontend/src/pages/Profile.jsx
+++ recipe-frontend/src/pages/Profile.jsx
@@ -1,5 +1,15 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth);
+  useEffect(() => {
+    if (!user) navigate("/login");
+  }, [user, navigate]);
+
+  return <div>Welcome {user && user.name}</div>;
 };
 
 export default Profile;
