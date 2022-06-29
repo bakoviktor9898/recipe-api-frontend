aebc0cabc4a38d5d867a28a3e7db9596586481f8
bako viktor <bako.viktor@promenade.hu>
2022-06-29 20:19:14 +0200

    remove some styling

--- recipe-frontend/src/components/Spinner.js
+++ recipe-frontend/src/components/Spinner.js
@@ -1,6 +1,6 @@
 export const Spinner = () => {
   return (
-    <div className="grid place-items-center h-full">
+    <div className="grid place-items-center">
       <svg
         role="status"
         className="w-24 h-24 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-teal-600"
