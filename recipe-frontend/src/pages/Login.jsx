9ebe842201bf14f052270ce13ef9e4afefa70770
bako viktor <bako.viktor@promenade.hu>
2022-06-29 20:16:16 +0200

    changed container height

--- recipe-frontend/src/pages/Login.jsx
+++ recipe-frontend/src/pages/Login.jsx
@@ -29,7 +29,7 @@ export const Login = () => {
   };
 
   return (
-    <div className="h-3/4">
+    <div className="h-[80vh]">
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
