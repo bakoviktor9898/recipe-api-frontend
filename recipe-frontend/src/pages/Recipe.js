1d1ec4b24faeab1a2dc1f49208e09f84a6ba990e
bako viktor <bako.viktor@promenade.hu>
2022-07-03 18:02:14 +0200

    remove not used store elements & add more margin to container

--- recipe-frontend/src/pages/Recipe.js
+++ recipe-frontend/src/pages/Recipe.js
@@ -7,9 +7,7 @@ import { ArrowCircleLeftIcon } from "@heroicons/react/outline";
 
 const Recipe = () => {
   const { id } = useParams();
-  const { recipe, isError, isSuccess, message, isLoading } = useSelector(
-    (state) => state.recipe
-  );
+  const { recipe, isLoading } = useSelector((state) => state.recipe);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -19,15 +17,15 @@ const Recipe = () => {
   if (isLoading) return <Spinner />;
 
   return (
-    <div className="mt-14 flex items-center justify-center">
+    <div className="mt-24 flex items-center justify-center">
       <button
         onClick={() => navigate(-1)}
-        className="absolute top-[115px] py-2 flex justify-center gap-1 items-center left-3  bg-teal-700 w-[87px] text-center text-white rounded-md"
+        className="absolute top-[115px] py-2 flex justify-center gap-1 items-center left-3  bg-teal-700 w-[127px] text-center text-white rounded-md"
       >
         <ArrowCircleLeftIcon className="w-7 h-7" />
         Vissza
       </button>
-      <div className="w-[70%] flex flex-col items-center gap-4 bg-white rounded-lg">
+      <div className="w-[90%] max-w-[1100px] flex flex-col items-center gap-4 bg-white rounded-lg">
         <h1 className="text-3xl pb-5 pt-8 text-center">{recipe?.name}</h1>
         <div className="text-xl">Kalória : {recipe?.kcal}</div>
         <div className="text-xl">
