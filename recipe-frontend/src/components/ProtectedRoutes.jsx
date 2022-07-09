5eb103e1fb6962e9f6af3a542f5be4d4c6b6a521
bako viktor <bako.viktor@promenade.hu>
2022-07-09 13:13:15 +0200

    change the if statement condition

--- recipe-frontend/src/components/ProtectedRoutes.jsx
+++ recipe-frontend/src/components/ProtectedRoutes.jsx
@@ -3,11 +3,10 @@ import { Navigate } from "react-router";
 
 const ProtectedRoutes = ({ children }) => {
   const { user } = useSelector((state) => state.auth);
-  if (!user) {
-    return <Navigate to="/login" />;
+  if (user) {
+    return children;
   }
-
-  return children;
+  return <Navigate to="/login" />;
 };
 
 export default ProtectedRoutes;
