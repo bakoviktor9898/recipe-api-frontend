c244f369a5241470e914d53e0174908160e716e0
bako viktor <bako.viktor@promenade.hu>
2022-07-09 20:57:26 +0200

    styling

--- recipe-frontend/src/components/RecipeItem.jsx
+++ recipe-frontend/src/components/RecipeItem.jsx
@@ -12,16 +12,16 @@ const RecipeItem = ({ recipe }) => {
   return (
     <Card
       shadow={false}
-      className="mt-6 rounded-xl bg-white font-medium m-4 w-[400px] h-96 flex-col items-center justify-evenly text-center"
+      className="mt-6 rounded-xl bg-white font-medium m-4 flex-col justify-center text-center"
     >
-      <CardHeader shadow={false} className="text-2xl ">
+      <CardHeader shadow={false} className="text-2xl" floated={false}>
         <span>{recipe.name}</span>
       </CardHeader>
-      <CardBody className="text-center mt-0">
+      <CardBody className="text-center">
         <div>Az elkészitesi idő csupán</div>
         <span>{recipe.preparation_time} perc</span>
       </CardBody>
-      <CardFooter className="text-center ">
+      <CardFooter className="text-center">
         <div>Ha szeretne megtudni hogyan tudja elkésziteni</div>
         <button
           className="bg-teal-700 p-2 rounded-md text-white mt-2"
