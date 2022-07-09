edf1113794782a8e721be0442b005e393857359f
bako viktor <bako.viktor@promenade.hu>
2022-07-09 12:59:40 +0200

    remove useEffect

--- recipe-frontend/src/App.js
+++ recipe-frontend/src/App.js
@@ -7,70 +7,61 @@ import Home from "./pages/Home";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Profile from "./pages/Profile";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Spinner } from "./components/Spinner";
-import { getUser, reset } from "./features/auth/authSlice";
 import PublicRoutes from "./components/PublicRoutes";
 import ProtectedRoutes from "./components/ProtectedRoutes";
 import Recipe from "./pages/Recipe";
 import NoMatch from "./pages/NoMatch";
+import PersistUser from "./components/PersistUser";
 
 function App() {
-  const dispatch = useDispatch();
-  const { isLoading } = useSelector((state) => state.auth);
-  useEffect(() => {
-    dispatch(getUser());
-  }, [dispatch]);
-
-  if (isLoading) return <Spinner />;
-
   return (
     <div className="App font-sans max-h-max min-h-screen bg-grey-200">
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route
-            path="/login"
-            element={
-              <PublicRoutes>
-                <Login />
-              </PublicRoutes>
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <PublicRoutes>
-                <Register />
-              </PublicRoutes>
-            }
-          />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoutes>
-                <Home />
-              </ProtectedRoutes>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoutes>
-                <Profile />
-              </ProtectedRoutes>
-            }
-          />
-          <Route
-            path="/recipe/:id"
-            element={
-              <ProtectedRoutes>
-                <Recipe />
-              </ProtectedRoutes>
-            }
-          />
-          <Route path="*" element={<NoMatch />} />
+          <Route element={<PersistUser />}>
+            <Route
+              path="/login"
+              element={
+                <PublicRoutes>
+                  <Login />
+                </PublicRoutes>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <PublicRoutes>
+                  <Register />
+                </PublicRoutes>
+              }
+            />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoutes>
+                  <Home />
+                </ProtectedRoutes>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoutes>
+                  <Profile />
+                </ProtectedRoutes>
+              }
+            />
+            <Route
+              path="/recipe/:id"
+              element={
+                <ProtectedRoutes>
+                  <Recipe />
+                </ProtectedRoutes>
+              }
+            />
+            <Route path="*" element={<NoMatch />} />
+          </Route>
         </Routes>
       </BrowserRouter>
       <ToastContainer />
